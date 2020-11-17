31283f778d934d43590a0e56c3a122700697ad48
Akshat jain <62347282+akshatjai258@users.noreply.github.com>
2020-11-17 17:05:01 +0530

    just want to switch branch

--- project/project/urls.py
+++ project/project/urls.py
@@ -19,6 +19,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+admin.site.site_header="Filox Admin"
+admin.site.site_title="Filox Admin Panel"
+admin.site.index_title="Welcome to Filox Admin Panel"
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('healthblog.urls')),
